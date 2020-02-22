78cb30f952f6cf065a472eacf26f326d5708ac87
IrinaLebedeva <ya.lebedeva.irina@gmail.com>
2020-02-22 23:09:13 +0300

    9: popups - проверка на наличие элементов на странице

--- js/script.js
+++ js/script.js
@@ -1,86 +1,109 @@
 "use strict";
 
-var contacts_link = document.querySelector(".contacts__link");
-var contacts_modal = document.querySelector(".modal .modal__feedback");
-var contacts_close = document.querySelector(".modal__feedback .modal__close");
-var contacts_form = contacts_modal.querySelector("form[name=feedback]");
-var contacts_field_name = contacts_form.querySelector("input[name=name]");
-var contacts_field_email = contacts_form.querySelector("input[name=email]");
-var contacts_field_body = contacts_form.querySelector("input[name=body]");
+(function() {
+  var contacts_link = document.querySelector(".contacts__link");
 
-var contacts_map_link = document.querySelector(".contacts__link-map");
-var contacts_map_modal = document.querySelector(".modal .modal__map");
-var contacts_map_close = document.querySelector(".modal__map .modal__close");
-
-var notice_cart_links = document.querySelectorAll(".goods__btn-buy");
-var notice_cart_modal = document.querySelector(".modal .modal__notice-cart");
-var notice_cart_close = document.querySelector(".modal__notice-cart .modal__close");
-var notice_cart_order = document.querySelector(".modal__notice-cart .notice-cart__order");
-var notice_cart_continue = document.querySelector(".modal__notice-cart .notice-cart__continue");
-
-/* modal: feedback fom */
-contacts_link.addEventListener("click", function (evt) {
-  evt.preventDefault();
-
-  closeModals();
-  contacts_modal.parentElement.classList.add("modal--active");
-  setTimeout(contacts_field_name.focus(), 1);
-
-});
-
-contacts_close.addEventListener("click", function (evt) {
-  evt.preventDefault();
-
-  closeModal(contacts_modal.parentElement);
-});
-
-contacts_form.addEventListener("submit", function (evt) {
-  if (!contacts_field_name.value ||
-      !contacts_field_email.value ||
-      !contacts_field_body.value) {
-
-    evt.preventDefault();
-    setTimeout('contacts_modal.parentElement.classList.add("modal--error");', 1);
-    contacts_modal.parentElement.classList.remove("modal--error");
+  if (contacts_link === null) {
+    return;
   }
-});
 
-/* modal: map */
-contacts_map_link.addEventListener("click", function (evt) {
-  evt.preventDefault();
+  var contacts_modal = document.querySelector(".modal .modal__feedback");
+  var contacts_close = document.querySelector(".modal__feedback .modal__close");
+  var contacts_form = contacts_modal.querySelector("form[name=feedback]");
+  var contacts_field_name = contacts_form.querySelector("input[name=name]");
+  var contacts_field_email = contacts_form.querySelector("input[name=email]");
+  var contacts_field_body = contacts_form.querySelector("input[name=body]");
 
-  closeModals();
-  contacts_map_modal.parentElement.classList.add("modal--active");
-});
-
-contacts_map_close.addEventListener("click", function (evt) {
-  evt.preventDefault();
-
-  closeModal(contacts_map_modal.parentElement);
-});
-
-/* modal: notice - item was added to the cart */
-notice_cart_links.forEach(function(item) {
-  item.addEventListener("click", function(evt) {
+  /* modal: feedback fom */
+  contacts_link.addEventListener("click", function (evt) {
     evt.preventDefault();
 
     closeModals();
-    notice_cart_modal.parentElement.classList.add("modal--active");
-    notice_cart_order.focus();
+    contacts_modal.parentElement.classList.add("modal--active");
+    setTimeout(contacts_field_name.focus(), 1);
+
   });
-});
 
-notice_cart_continue.addEventListener("click", function (evt) {
-  evt.preventDefault();
+  contacts_close.addEventListener("click", function (evt) {
+    evt.preventDefault();
 
-  closeModal(notice_cart_modal.parentElement);
-});
+    closeModal(contacts_modal.parentElement);
+  });
 
-notice_cart_close.addEventListener("click", function (evt) {
-  evt.preventDefault();
+  contacts_form.addEventListener("submit", function (evt) {
+    if (!contacts_field_name.value ||
+      !contacts_field_email.value ||
+      !contacts_field_body.value) {
 
-  closeModal(notice_cart_modal.parentElement);
-});
+      evt.preventDefault();
+      setTimeout('contacts_modal.parentElement.classList.add("modal--error");', 1);
+      contacts_modal.parentElement.classList.remove("modal--error");
+    }
+  });
+
+})();
+
+(function () {
+  var contacts_map_link = document.querySelector(".contacts__link-map");
+
+  if (contacts_map_link === null) {
+    return;
+  }
+
+  var contacts_map_modal = document.querySelector(".modal .modal__map");
+  var contacts_map_close = document.querySelector(".modal__map .modal__close");
+
+  /* modal: map */
+  contacts_map_link.addEventListener("click", function (evt) {
+    evt.preventDefault();
+
+    closeModals();
+    contacts_map_modal.parentElement.classList.add("modal--active");
+  });
+
+  contacts_map_close.addEventListener("click", function (evt) {
+    evt.preventDefault();
+
+    closeModal(contacts_map_modal.parentElement);
+  });
+
+})();
+
+(function () {
+  if (document.querySelector(".goods__btn-buy") === null) {
+    return;
+  }
+
+  var notice_cart_links = document.querySelectorAll(".goods__btn-buy");
+  var notice_cart_modal = document.querySelector(".modal .modal__notice-cart");
+  var notice_cart_close = document.querySelector(".modal__notice-cart .modal__close");
+  var notice_cart_order = document.querySelector(".modal__notice-cart .notice-cart__order");
+  var notice_cart_continue = document.querySelector(".modal__notice-cart .notice-cart__continue");
+
+  /* modal: notice - item was added to the cart */
+  notice_cart_links.forEach(function(item) {
+    item.addEventListener("click", function(evt) {
+      evt.preventDefault();
+
+      closeModals();
+      notice_cart_modal.parentElement.classList.add("modal--active");
+      notice_cart_order.focus();
+    });
+  });
+
+  notice_cart_continue.addEventListener("click", function (evt) {
+    evt.preventDefault();
+
+    closeModal(notice_cart_modal.parentElement);
+  });
+
+  notice_cart_close.addEventListener("click", function (evt) {
+    evt.preventDefault();
+
+    closeModal(notice_cart_modal.parentElement);
+  });
+
+})();
 
 /* modal close */
 window.addEventListener("keydown", function (evt) {
